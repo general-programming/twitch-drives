c37bcaec58b1fa55accbb48f88e2e6d5e461eba1
nepeat <nepeat@gmail.com>
2021-11-14 08:42:26 +0000

    service - move grabber to service_grabber

--- service_grabber/__main__.py
+++ service_grabber/__main__.py
@@ -2,11 +2,6 @@ import asyncio
 from twitchdrives.api.tesla import get_tesla
 
 class VehicleGrabber:
-    @property
-    @staticmethod
-    def loop():
-        return asyncio.get_event_loop()
-
     async def main(self):
         self.tesla = await get_tesla()
 
