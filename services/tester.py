181c48e0d48ae32d6d52bc537d1b52b569388baa
nepeat <nepeat@gmail.com>
2021-11-14 14:32:22 +0000

    tester - random battery + range

--- services/tester.py
+++ services/tester.py
@@ -9,6 +9,8 @@ redis = get_redis()
 while True:
     redis.publish("tesla:state", json.dumps({
         "speed": random.randint(0, 420),
-        "shift_state": random.choice("PRND")
+        "shift_state": random.choice("PRND"),
+        "battery": random.randint(0, 101),
+        "range": random.randint(0, 300),
     }))
-    time.sleep(0.25)
+    time.sleep(1)
