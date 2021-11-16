be22378d2d5ab73c8bcf4c1843bdf3adab5ef0b2
nepeat <nepeat@gmail.com>
2021-11-16 17:44:41 +0000

    overlay - react template start

---
+++ frontend_overlay/src/index.js
@@ -0,0 +1,12 @@
+import './style';
+import TeslaStore from './components/TeslaStore';
+
+export default function App() {
+	return (
+		<TeslaStore>
+			<div>
+				<h1>Hello, World!</h1>
+			</div>
+		</TeslaStore>
+	);
+}
