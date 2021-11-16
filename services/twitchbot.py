e2005325728716010cca46a7433c535465967518
nepeat <nepeat@gmail.com>
2021-11-16 08:18:39 +0000

    twitch - use twitchio for connecting to twitch

--- services/twitchbot.py
+++ services/twitchbot.py
@@ -1,7 +1,6 @@
-import asyncio
 import os
 
-import bottom
+from twitchio.ext import commands
 
 from twitchdrives.api.tesla import get_car
 from twitchdrives.caractions.navigation import NavigationAction
@@ -10,63 +9,52 @@ from twitchdrives.store.chat import ChatStore
 
 nick = os.environ["TWITCH_USER"]
 server = "irc.chat.twitch.tv"
-password = os.environ["TWITCH_PASS"]
 port = 6697
 
-bot = bottom.Client(host=server, port=port, ssl=True)
 chatstore = ChatStore()
 
 
-@bot.on("CLIENT_CONNECT")
-async def connect(**kwargs):
-    print("connecting")
-    bot.send("PASS", password=password)
-    bot.send("NICK", nick=nick)
-    bot.send("USER", user=nick, realname="https://github.com/numberoverzero/bottom")
+class Bot(commands.Bot):
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        super().__init__(
+            token=os.environ["TWITCH_PASS"], prefix="!", initial_channels=["#" + nick]
+        )
 
-    # Don't try to join channels until the server has
-    # sent the MOTD, or signaled that there's no MOTD.
-    print("waiting for motd")
-    done, pending = await asyncio.wait(
-        [bot.wait("RPL_ENDOFMOTD"), bot.wait("ERR_NOMOTD")],
-        loop=bot.loop,
-        return_when=asyncio.FIRST_COMPLETED,
-    )
-    print("motd done")
+    async def event_ready(self):
+        # We are logged in and ready to chat and use commands...
+        print(f"Logged in as | {self.nick}")
 
-    # Cancel whichever waiter's event didn't come in.
-    for future in pending:
-        future.cancel()
+    async def event_message(self, message):
+        if message.author:
+            author_name = message.author.name
+        else:
+            author_name = self.nick
 
-    bot.send("JOIN", channel="#" + nick)
-    print("joined channel")
+        await chatstore.add(
+            source="twitch",
+            message=message.content,
+            nick=author_name,
+            channel=message.channel.name,
+        )
+        print(author_name, message.channel.name, message.content)
 
+        # Messages with echo set to True are messages sent by the bot...
+        # For now we just want to ignore them...
+        if message.echo:
+            return
 
-@bot.on("client_disconnect")
-async def reconnect(**kwargs):
-    await bot.connect()
+        # Since we have commands and are overriding the default `event_message`
+        # We must let the bot know we want to handle and invoke our commands...
+        await self.handle_commands(message)
 
-
-@bot.on("ping")
-def keepalive(message, **kwargs):
-    bot.send("PONG", message=message)
-
-
-@bot.on("PRIVMSG")
-async def message(nick: str, target: str, message: str, **kwargs):
-    """Echo all messages"""
-    print(nick, target, message)
-    await chatstore.add("twitch", message, nick, target)
-
-    message = message.strip()
-    if message.startswith("!info"):
+    @commands.command()
+    async def info(self, ctx: commands.Context):
         async with get_car() as car:
             try:
                 car_details = await car.get_vehicle_data()
             except VehicleAsleep:
-                bot.send(
-                    "PRIVMSG", target=target, message="The car is in sleep mode. :("
-                )
+                await ctx.send("The car is in sleep mode. :(")
                 return
 
             output = "{name} is at {charge}%, going {speed}mph. The temperature outside is {outside_temp}C. The temperature inside is {inside_temp}C. Car's version is {version}".format(
@@ -77,22 +65,22 @@ async def message(nick: str, target: str, message: str, **kwargs):
                 outside_temp=car_details["climate_state"]["outside_temp"],
                 version=car_details["vehicle_state"]["car_version"],
             )
-            bot.send("PRIVMSG", target=target, message=output)
-    elif message.startswith("!navigate"):
-        try:
-            destination = message.split("!navigate", 2)[1]
-        except IndexError:
-            return
+
+            await ctx.send(output)
+
+    @commands.command()
+    async def navigate(self, ctx: commands.Context, *args):
+        location = " ".join(args)
         navigation = NavigationAction()
 
         try:
-            await navigation.handle(destination)
-            reply = f"Navigating to '{destination}'"
+            await navigation.handle(location)
+            reply = f"Navigating to '{location}'"
         except VehicleInvalidShare:
-            reply = f"'{destination}' is not a valid destination."
+            reply = f"'{location}' is not a valid destination."
 
-        bot.send("PRIVMSG", target=target, message=reply)
+        await ctx.send(reply)
 
 
-bot.loop.create_task(bot.connect())
-bot.loop.run_forever()
+bot = Bot()
+bot.run()
