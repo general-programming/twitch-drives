b730255bac07938b35ca4c5b96a65687e350f90e
tthbaltazar <44551289+tthbaltazar@users.noreply.github.com>
2021-11-17 06:21:32 +0100

    SocketProvider - cleanup

--- frontend_overlay/src/features/SocketProvider.js
+++ frontend_overlay/src/features/SocketProvider.js
@@ -1,49 +1,33 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { socketUpdate } from './driveinfo/driveInfoSlice';
 import { addMessage } from './chat/chatSlice';
 
-const onOpen = (event) => {
-    console.log("socket opened", event);
-}
-
-const onMessage = (dispatch, event) => {
-    const data = JSON.parse(event.data);
-    if (data.chat) {
-        dispatch(addMessage(data.chat));
-    } else {
-        dispatch(socketUpdate(data));
-    }
-}
-
-function createSocket(dispatch, onMessage) {
-    // socket = new WebSocket("ws://" + document.location.host + "/stream");
-    let socket = new WebSocket("ws://79.110.170.251:8000/stream");
-    socket.onopen = onOpen;
-    socket.onmessage = onMessage;
-    socket.onclose = (event) => {
-        console.log("socket closed", event);
-        if (event.code !== 4269) {
-            setTimeout(() => createSocket(dispatch, onMessage), 1000);
-        }
-    };
-
-    return socket;
-}
-
-const SocketProvider = ({ children }) => {
+export default function SocketProvider({ children }) {
     const dispatch = useDispatch();
-    const [socket, setSocket] = useState(0);
+    const [serial, setSerial] = useState(0);
 
     useEffect(() => {
-        let socket = createSocket(dispatch, (event) => onMessage(dispatch, event));
+        let socket = new WebSocket("ws://79.110.170.251:8000/stream");
+        socket.onopen = (event) => {
+            console.log("socket opened", event);
+        };
+        socket.onclose = (event) => {
+            console.log("socket closed", event);
+            setSerial(x => x+1); // trigger re-render
+        };
 
-        return () => {
-            socket.close(4269);
-        }
-    });
+        socket.onmessage = event => {
+            const data = JSON.parse(event.data);
+            if (data.chat) {
+                dispatch(addMessage(data.chat));
+            } else {
+                dispatch(socketUpdate(data));
+            }
+        };
+
+        return () => socket.close();
+    }, [serial]);
 
     return children;
 };
-
-export default SocketProvider;
