6413e4524b12e85cb791ccabf452224ba8cf6dd9
tthbaltazar <44551289+tthbaltazar@users.noreply.github.com>
2021-11-19 11:51:46 +0100

    redux - remove example counter slice

--- frontend_overlay/src/app/store.js
+++ frontend_overlay/src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import driveInfoReducer from "../features/driveinfo/driveInfoSlice";
 import chatReducer from "../features/chat/chatSlice";
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
         driveInfo: driveInfoReducer,
         chat: chatReducer,
     },
