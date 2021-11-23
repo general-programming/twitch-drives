77019a1e51412330fe44e697abc4cb2c6e446076
nepeat <nepeat@gmail.com>
2021-11-23 04:28:44 +0000

    overlay - bad react practices for map icon

--- frontend_overlay/src/features/map/Map.js
+++ frontend_overlay/src/features/map/Map.js
@@ -12,30 +12,41 @@ import "leaflet/dist/leaflet.css";
 import * as L from "leaflet";
 
 // Setup map
-delete L.Icon.Default.prototype._getIconUrl;
+// delete L.Icon.Default.prototype._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
-    iconUrl: require("leaflet/dist/images/marker-icon.png").default,
-    shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+// L.Icon.Default.mergeOptions({
+//     iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png").default,
+//     iconUrl: require("leaflet/dist/images/marker-icon.png").default,
+//     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+// });
+
+const arrowIcon = L.divIcon({
+    className: 'icon-arrow',
+    html: '<svg viewBox="0 0 32 32" height="32" width="32"><path style="fill: #9146ff" d="m 15.992188,2 a 1.0001,1.0001 0 0 0 -0.914063,0.6113281 l -11,25.9999999 A 1.0001,1.0001 0 0 0 5.5371094,29.84375 L 16,23.185547 26.462891,29.84375 a 1.0001,1.0001 0 0 0 1.458984,-1.232422 l -11,-25.9999999 A 1.0001,1.0001 0 0 0 15.992188,2 Z" /></svg>',
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
 });
 
 const mapboxToken =
     "pk.eyJ1IjoibmVwZWF0IiwiYSI6ImNrdnVmYWZrOTVhNGQydW51aTZ0eHVpYmcifQ.16at8c1_HQ2xUmMn1R3JBQ";
 const startZoom = 15;
 
-function ChangeView({ latitude, longitude }) {
+function ChangeView({ latitude, longitude, heading, marker }) {
     const map = useMap();
     const location = new L.LatLng(latitude, longitude);
     map.panTo(location);
+    try {
+        document.getElementsByClassName("icon-arrow")[0].children[0].style["transform"] = `rotate(${heading}deg)`
+    } catch (e) { }
     // mapMarker.setLatLng(location)
 
-    return null;
+    return marker;
 }
 
 export function Map() {
     const info = useSelector(selectDriveInfo);
     const position = [info.latitude, info.longitude];
+    const marker = <Marker position={position} icon={arrowIcon} />;
 
     return (
         <MapContainer
@@ -54,8 +65,8 @@ export function Map() {
                 zoomOffset={-1}
                 accessToken={mapboxToken}
             />
-            <Marker position={position} />
-            <ChangeView latitude={info.latitude} longitude={info.longitude} />
+
+            <ChangeView latitude={info.latitude} longitude={info.longitude} heading={info.heading} marker={marker} />
         </MapContainer>
     );
 }
