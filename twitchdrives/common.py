533ddbbaf95830ead150a855d66a2d2090c009c2
nepeat <nepeat@gmail.com>
2021-11-23 04:26:57 +0000

    common - aioredis context manager

--- twitchdrives/common.py
+++ twitchdrives/common.py
@@ -1,4 +1,6 @@
+from contextlib import asynccontextmanager
 import os
+from typing import AsyncGenerator
 
 import aioredis
 import redis
@@ -12,3 +14,12 @@ def get_redis() -> redis.Redis():
 
 def get_aioredis():
     return aioredis.from_url(REDIS_URL, decode_responses=True)
+
+
+@asynccontextmanager
+async def ctx_aioredis() -> AsyncGenerator[aioredis.Redis, None]:
+    try:
+        redis = await get_aioredis()
+        yield redis
+    finally:
+        await redis.close()
