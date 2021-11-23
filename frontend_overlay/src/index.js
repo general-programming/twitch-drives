02355c00da9cecda50d675ef096ff9291900c750
nepeat <nepeat@gmail.com>
2021-11-23 04:07:57 +0000

    overlay - use font that is more available

--- frontend_overlay/src/index.js
+++ frontend_overlay/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "@fontsource/montserrat";
 import "normalize.css";
 import App from "./App";
 import { store } from "./app/store";
